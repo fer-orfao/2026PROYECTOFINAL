5c2332a9e8b334638a41dda21b3a71667e6ae2ef
fer-orfao <133964332+fer-orfao@users.noreply.github.com>
2026-06-29 19:12:47 +0000

    origin/feat-autenticacion

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS  # <-- AÑADE ESTA LÍNEA
 
 # from models import Person
 
@@ -18,6 +19,7 @@ ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
+CORS(app)
 app.config["JWT_SECRET_KEY"] = "super-secret-key-pokemon"
 app.url_map.strict_slashes = False
 
